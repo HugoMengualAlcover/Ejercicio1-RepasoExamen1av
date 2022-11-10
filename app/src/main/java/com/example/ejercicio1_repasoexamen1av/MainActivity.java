96e449684e230fdfe203a7e71b7638f5a571573d
Hugo <hugome2307@gmail.com>
2022-11-10 12:33:08 +0100

    Image

--- app/src/main/java/com/example/ejercicio1_repasoexamen1av/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1_repasoexamen1av/MainActivity.java
@@ -7,13 +7,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ejercicio1_repasoexamen1av.databinding.ActivityMainBinding;
+
+import java.text.NumberFormat;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private final int NUMINICIALCIGARROS = 20;
+    private int numCigarros;
+    private int dineroAhorrado;
+
+    private NumberFormat nf = NumberFormat.getCurrencyInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        numCigarros = NUMINICIALCIGARROS;
+
+        binding.contentMain.lblDineroAhorradoMain.setText("Cigarrillos restantes " + numCigarros);
+
+        binding.contentMain.btnNoFumarMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                numCigarros -= 1;
+                if(numCigarros<1){
+                    numCigarros = NUMINICIALCIGARROS;
+                    dineroAhorrado += 5;
+                    Toast.makeText(MainActivity.this, "Te has acabado una caja, y te has ahorrado " + nf.format(5), Toast.LENGTH_SHORT).show();
+                    binding.contentMain.lblDineroAhorradoMain.setText("Dinero ahorrado "+ nf.format(dineroAhorrado));
+                }
+                binding.contentMain.lblNumCigarrosMain.setText("Cigarrillos restantes " + numCigarros);
             }
         });
     }
